430f770d7622a5253ec4a79af490b4fbbb24910e
Marcelo HP Ferreira <marcelohpf@gmail.com>
2018-08-30 11:54:32 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 NAME="Marcelo Ferreira"
 EMAIL="marcelohpf@gmail.com"
 
-yes S | sudo pacman -Suy git curl tree htop vim evince xclip
+yes S | sudo pacman -Suy git curl tree htop neovim evince xclip
 
 git config --global user.name "$NAME"
 git config --global user.email $EMAIL
-git config --global core.editor vim
+git config --global core.editor nvim
 
+# Install nvm to manage node and npm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+# Install Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sudo pip3 install --upgrade pip
 pip3 install virtualenv ipython --user
